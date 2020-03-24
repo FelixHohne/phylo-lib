9982916623e6cc40a1bd4375f8f82df5abd5df1c
Your Name <fmh42@cornell.edu>
2020-03-24 15:44:03 +0100

    Added documentation and improved file readability

--- tree.ml
+++ tree.ml
@@ -1,6 +1,7 @@
 type clade_id = int
 exception UnknownClade of clade_id
 
+(** Represents a node in the phylogenetic tree *)
 type node = Clade of {
     clade_id : clade_id;
     bootstrap : float option;
@@ -13,15 +14,18 @@ type t = Node of (node * t list)
 
 let empty = Node (Clade { clade_id = 0; bootstrap = None }, [])
 
+
 let rec add_species (tree : t) (clade_id : clade_id) (species : string) : t = 
   match tree with
+  | Node (Leaf _, lst) -> raise (UnknownClade clade_id)
   | Node (Clade info, lst) -> 
     if info.clade_id = clade_id 
-    then Node (Clade info, Node (Leaf {species = species}, [])::lst)
+    then let new_species = Leaf {species = species} in 
+         let new_sub_tree = Node (new_species, []) in 
+         Node (Clade info, new_sub_tree::lst)
     else add_species_helper lst clade_id species
-  | Node (Leaf _, lst) -> raise (UnknownClade clade_id)
 and 
-  add_species_helper (lst: t list) (clade_id : clade_id) (species : string) : t =
+add_species_helper (lst: t list) (clade_id : clade_id) (species : string) : t =
   match lst with
   | [] -> raise (UnknownClade clade_id)
   | h::t -> try add_species h clade_id species
