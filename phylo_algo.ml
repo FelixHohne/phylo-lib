d16c271fe9db8ff34d02e0857ab587aa9781e7da
Vaishnavi Gupta <vg222@cornell.edu>
2020-05-04 15:55:50 -0400

    code quality + documentation for phylo_algo

--- phylo_algo.ml
+++ phylo_algo.ml
@@ -11,7 +11,8 @@ let rec add_species species i j acc =
   let s2 = species.(j) in
   match (List.assoc_opt i acc), (List.assoc_opt j acc) with
   | None, None -> 
-    ((Stdlib.min i j), zip_no_params [leaf_no_params s1; leaf_no_params s2])::acc
+    ((Stdlib.min i j), zip_no_params [leaf_no_params s1; leaf_no_params s2])
+    ::acc
   | Some t, None -> let r = List.remove_assoc i acc in 
     ((Stdlib.min i j ), zip_no_params [leaf_no_params s2; t])::r
   | None, Some t -> let r = List.remove_assoc j acc in 
@@ -20,23 +21,30 @@ let rec add_species species i j acc =
     let r = acc |> List.remove_assoc i |> List.remove_assoc j in 
     ((Stdlib.min i j), zip_no_params [t1; t2])::r
 
+(** [upgma_help dist species acc] is the phylogenetic tree that results from
+    running the UPGMA algorithm on [dist] with species names [species] and 
+    zipping up the results with [acc].
+
+    Preconditions: if [acc] is empty, then [is_done dist] must be false, i.e. 
+    there must be more than one remaining column in distance matrix [dist].
+    For every column index in [dist] there must be a corresponding name in 
+    [species].
+*)
 let rec upgma_help dist species acc = 
-  (* print_endline ("upgma_help called"); *)
   if is_done dist then 
-    ( (* print_endline ("done dist"); *)
+    begin
       let tree_list = (List.rev_map (fun x -> snd x) acc) in
       if List.length tree_list = 1 then (List.hd tree_list)
       else if List.length tree_list > 1 then zip_no_params tree_list
-      else failwith "Precondition violated in upgma_help")
+      else failwith "Precondition violated in upgma_help"
+    end
   else
-    ( (* print_endline ("not done dist"); *)
+    begin
       let i, j = min_index dist in
-      (* print_endline ("min_index dist called"); *)
       let t = add_species species i j acc in
-      (* print_endline ("added species"); *)
       let d = combine i j dist in 
-      (* print_endline ("combined"); *)
-      upgma_help d species t)
+      upgma_help d species t
+    end
 
 let upgma dist species = 
   match Array.length species with
