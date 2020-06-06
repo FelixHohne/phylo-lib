048a2f303c46439762441db8e0ac4499f55562fe
Shiyuan-Huang-23 <shiyuancn@live.cn>
2020-06-05 22:35:45 -0400

    Started implementing phyloXML printing

--- phylo_printer_test.ml
+++ phylo_printer_test.ml
@@ -9,3 +9,18 @@ let phylo_printer_tests =
   "test suite for parser" >::: List.flatten [
     trees
   ]
+
+(* 
+#mod_use "tree.ml";;
+#mod_use "phylo_parser.ml";;
+#mod_use "phylo_printer.ml";;
+let phylo = Phylo_parser.from_phylo "PhyloXML/collapsed_tree.xml";;
+let tree = phylo.tree;;
+Phylo_printer.print_tree_xml tree "testfile.xml";;
+let phylo2 = Phylo_parser.from_phylo "PhyloXML/expanded_tree.xml";;
+let tree2 = phylo2.tree;;
+Phylo_printer.print_tree_xml tree2 "testfile2.xml";;
+let phylo3 = Phylo_parser.from_phylo "testfile2.xml";;
+Tree.print_tree tree2;;
+Tree.print_tree phylo3.tree;;
+*)
