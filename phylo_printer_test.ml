f092cdf10c813de68a950500f1a6e59093a3ad4d
Shiyuan-Huang-23 <shiyuancn@live.cn>
2020-06-10 19:53:28 -0400

    Fix duplicate clade bug

--- phylo_printer_test.ml
+++ phylo_printer_test.ml
@@ -11,12 +11,16 @@ let phylo_printer_tests =
   ]
 
 (* 
-#mod_use "tree.ml";;
-#mod_use "phylo_parser.ml";;
-#mod_use "phylo_printer.ml";;
-let phylo = Phylo_parser.from_phylo "PhyloXML/collapsed_tree.xml";;
-let tree = phylo.tree;;
-Phylo_printer.print_tree_xml tree "testfile.xml";;
+let phylo = Phylo_parser.from_phylo "PhyloXML/small_tree.xml";;
+Phylo_printer.print_tree_xml phylo.tree "testfile.xml";;
+Tree.print_tree phylo.tree;;
+
+let phylo2 = Phylo_parser.from_phylo "PhyloXML/collapsed_tree.xml";;
+Phylo_printer.print_tree_xml phylo2.tree "testfile2.xml";;
+Tree.print_tree phylo2.tree;;
+let phylo_printed = Phylo_parser.from_phylo "testfile.xml";;
+Tree.print_tree phylo_printed.tree;;
+
 let phylo2 = Phylo_parser.from_phylo "PhyloXML/expanded_tree.xml";;
 let tree2 = phylo2.tree;;
 Phylo_printer.print_tree_xml tree2 "testfile2.xml";;
